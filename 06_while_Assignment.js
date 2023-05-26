5de297556a095eed6e8ca200a79e53f4d2db3d67
140122121994 <anandpjadhav143@gmail.com>
2023-05-26 22:41:18 +0530

    Assignment on While
    creating table & find even and odd number in given string.

---
+++ 06_while_Assignment.js
@@ -0,0 +1,54 @@
+console.log(`......Program to print 5 to 10 incrementing number.......`);
+let i = 5;
+while (i<=15) {
+    console.log(i);
+    i = i+1;
+}
+
+console.log(`......Program to print 50 to 40 Decrementing number.......`);
+let k = 50;
+while (k>=40) {
+    console.log(k);
+    k--;
+}
+
+console.log(`......Program to print odd Number.......`);
+let j = 1 ;
+while (j<=15) {
+    if (j%2==1) {
+        console.log(j);
+        
+    }
+    j++;
+}
+
+console.log(`......Program to print Even Number.......`);
+let m = 0;
+while (m<=10) {
+    if (m%2==0) {
+    console.log(m);
+        }
+     m++;    
+}
+
+console.log(`......Program to print Table of 5.......`);
+let s = 5 ;
+while (s<=50) {
+    console.log(s);
+    s = s+5;   
+}
+
+console.log(`......Program to print Table of 10.......`);
+let n = 10;
+while (n<=100) {
+    console.log(n);
+    n=n+10;
+    
+}
+
+console.log(`......Program to print Reverse Table of 10.......`);
+let a = 100 ;
+while (a>=10) {
+    console.log(a);
+    a = a-10;
+}
